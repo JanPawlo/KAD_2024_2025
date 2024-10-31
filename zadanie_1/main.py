a8cd458c751386cdbb78a853e11c2db7cf308f2b
Emil Franczak <251511@edu.p.lodz.pl>
2024-10-31 18:43:35 +0100

    Add main

---
+++ zadanie_1/main.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 17:17:31 2024
+
+@author: Jan Pawłowski
+@author: Emil Franczak
+"""
+
+from zadanie_1 import fileLoader, countThreeSpecies, speciesShareOfPopulation, getMinimumTraits, getAverageTraits, getMedianTraits, getMaximumTraits, getQuartilesTraits
+
+def main():
+    print("---- Zadanie 1 ----")
+    
+    data = fileLoader("data1.csv")
+    
+    
+    print()
+    print("Tabela 1. Liczności gatunku irysów.")
+    
+    # two dictionaries with species names as keys
+    # one containing species count and the other species share
+    species_count = countThreeSpecies(data)
+    species_share = speciesShareOfPopulation(species_count)
+    
+    #printing the table (species name;species count(spiecies share))
+    print("Setosa;" +  str(species_count["setosa"]) +"("+ str(round(species_share["setosa"]*100, 1))+"%)")
+    print("Versicolor;" + str(species_count["versicolor"]) +"("+ str(round(species_share["versicolor"]*100, 1))+"%)")
+    print("Virginica;" + str(species_count["virginica"]) +"("+ str(round(species_share["virginica"]*100, 1))+"%)")
+    print("Total;"+ str(len(data))+"(100.0%)")
+    
+    
+    print()
+    print("Tabela 2. Charakterystyka cech irysów.")
+        
+    minimum = getMinimumTraits(data)
+    maximum = getMaximumTraits(data)
+    average = getAverageTraits(data)
+    median = getMedianTraits(data)
+    quartiles = getQuartilesTraits(data)
+    
+    print("Długość działki kielicha (cm);" + str(round(minimum["sepal_length"], 2)) + ";" + str(round(average["sepal_length"], 2)) + "(+-TBD);" + str(round(median["sepal_length"], 2)) + "(" + str(round(quartiles["sepal_length"][0], 2)) + " - " + str(round(quartiles["sepal_length"][2], 2)) + ");" + str(round(maximum["sepal_length"], 2)))
+    print("Szerokość działki kielicha (cm);" + str(round(minimum["sepal_width"], 2)) + ";" + str(round(average["sepal_width"], 2)) + "(+-TBD);" + str(round(median["sepal_width"], 2)) + "(" + str(round(quartiles["sepal_width"][0], 2)) + " - " + str(round(quartiles["sepal_width"][2], 2)) + ");" + str(round(maximum["sepal_width"], 2)))
+    print("Długość płatka (cm);" + str(round(minimum["petal_length"], 2)) + ";" + str(round(average["petal_length"], 2)) + "(+-TBD);" + str(round(median["petal_length"], 2)) + "(" + str(round(quartiles["petal_length"][0], 2)) + " - " + str(round(quartiles["petal_length"][2], 2)) + ");" + str(round(maximum["petal_length"], 2)))
+    print("Szerokość płatka (cm));" + str(round(minimum["petal_width"], 2)) + ";" + str(round(average["petal_width"], 2)) + "(+-TBD);" + str(round(median["petal_width"], 2)) + "(" + str(round(quartiles["petal_width"][0], 2)) + " - " + str(round(quartiles["petal_width"][2], 2)) + ");" + str(round(maximum["petal_width"], 2)))
+
+    
+    
+    
+
+    
+    
+
+main()
+    
