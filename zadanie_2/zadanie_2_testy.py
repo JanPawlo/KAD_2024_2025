45abbc6a1487c6fa2652be6fb6600bb5408ba57d
251608 <251608@edu.p.lodz.pl>
2024-11-29 13:21:19 +0100

    finished euclideanDistance test

--- zadanie_2/zadanie_2_testy.py
+++ zadanie_2/zadanie_2_testy.py
@@ -12,26 +12,45 @@ def generate2randomPoints():
     pointA = list()
     pointB = list()
     for i in range(dimensions):
-        pointA.append(random.random())
-        pointB.append(random.random())
+        pointA.append(random.uniform(-0.5, 0.5))
+        pointB.append(random.uniform(-0.5, 0.5))
     return (pointA, pointB)
 
-
+# simple function that rounds result before comparing it with the compared value
+# by default we expect them to be equal
 def check(val1, val2, rounding:int=5, expectedResult:bool=True):
     print( (round(val1, rounding) == round(val2, rounding)) == expectedResult)
 
+
+
 def testEuclideanDistance():
+    print("Test Euclidean Distance")
     point0 = (0, 0)
     point1 = (1, 1)
     
     square_side = random.random()
     pointSQ = (square_side, square_side)
     
-    
-    
+    #basic
     check(euclideanDistance(point0, point1), math.sqrt(2))
     check(euclideanDistance(point0, pointSQ), math.sqrt(2)*square_side)
     
+    
+    #random
+    pointN, pointM = generate2randomPoints();
+    expectedDistance = 0
+    for i in range(len(pointN)):
+        expectedDistance += (pointN[i] - pointM[i])**2
+    expectedDistance = math.sqrt(expectedDistance)
+    
+    #check normal
+    check(euclideanDistance(pointN, pointM), expectedDistance)
+    #check reversed
+    check(euclideanDistance(pointN, pointM), euclideanDistance(pointM, pointN))
+
+    
+    
+    print()
     
     
 testEuclideanDistance()
