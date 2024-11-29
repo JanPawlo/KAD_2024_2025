7867dc42967b9170209b033e7d7a28037de615aa
251608 <251608@edu.p.lodz.pl>
2024-11-29 13:02:41 +0100

    fixed distance tests

--- zadanie_2/zadanie_2_testy.py
+++ zadanie_2/zadanie_2_testy.py
@@ -6,7 +6,7 @@ from zadanie_2 import *
 
 
 def generate2randomPoints():
-    dimensions = random.randint%20 + 2
+    dimensions = random.randint(2, 20)
     
     
     pointA = list()
@@ -17,15 +17,20 @@ def generate2randomPoints():
     return (pointA, pointB)
 
 
-def check(val1, val2, expectedResult:bool=True):
-    print( (round(val1, 3) == round(val2), 3) == expectedResult)
+def check(val1, val2, rounding:int=5, expectedResult:bool=True):
+    print( (round(val1, rounding) == round(val2, rounding)) == expectedResult)
 
 def testEuclideanDistance():
     point0 = (0, 0)
     point1 = (1, 1)
     
-    print(euclideanDistance(point0, point1), math.sqrt(2))
-    check( euclideanDistance(point0, point1), math.sqrt(2))
+    square_side = random.random()
+    pointSQ = (square_side, square_side)
+    
+    
+    
+    check(euclideanDistance(point0, point1), math.sqrt(2))
+    check(euclideanDistance(point0, pointSQ), math.sqrt(2)*square_side)
     
     
     
