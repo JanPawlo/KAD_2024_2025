710da05ea5bd98674133645ff16f4fe46c1274b9
Emil Franczak <251511@edu.p.lodz.pl>
2024-12-03 23:04:17 +0100

    Displaying scatter plots with grouping

---
+++ zadanie_2/main.py
@@ -0,0 +1,54 @@
+import utility as U #utility commands, such as fileLoader, splitList
+# import seaborn as sns
+from zadanie_2 import *
+import matplotlib.pyplot as plt
+
+
+def main():
+    
+    data = U.fileLoader("data2.csv")
+    normalized_data = minMaxScaling(data)
+    
+    
+    # finding the best WCSS (potentially could be implemented as function)
+    clusters, centroids = groupWithKcentroids(normalized_data, 3)
+    current_WCSS = WCSS(centroids, clusters, normalized_data)
+    for i in range(10):
+        new_clusters, new_centroids = groupWithKcentroids(normalized_data, 3)
+        if(current_WCSS > WCSS(new_centroids, new_clusters, normalized_data)):
+            clusters, centroids = new_clusters, new_centroids
+            
+    
+    # Displaying the scatter plots with grouping
+    figure, axis = plt.subplots(3, 2, figsize=(10, 14)) # rows, columns
+    figure.tight_layout(pad=4.0) # adjusting padding between plots
+
+    generateScatterPlot(data, 0, 1, axis[0][0], clusters, centroids)
+    axis[0][0].set_xlabel("Długość działki kielicha (cm)") # Dlugosc dzialki kielicha w 1
+    axis[0][0].set_ylabel("Szerokość działki kielicha (cm)") #szerokosc dzialki kielicha w 1 
+    
+    generateScatterPlot(data, 0, 2, axis[0][1], clusters, centroids)
+    axis[0][1].set_xlabel("Długość działki kielicha (cm)") 
+    axis[0][1].set_ylabel("Długość płatka (cm)")
+    
+    generateScatterPlot(data, 0, 3, axis[1][0], clusters, centroids)
+    axis[1][0].set_xlabel("Długość działki kielicha (cm)") 
+    axis[1][0].set_ylabel("Szerokość płatka (cm)")
+    
+    generateScatterPlot(data, 1, 2, axis[1][1], clusters, centroids)
+    axis[1][1].set_xlabel("Szerokość działki kielicha (cm)") 
+    axis[1][1].set_ylabel("Długość płatka (cm)")
+    
+    generateScatterPlot(data, 1, 3, axis[2][0], clusters, centroids)
+    axis[2][0].set_xlabel("Szerokość działki kielicha (cm)") 
+    axis[2][0].set_ylabel("Szerokość płatka (cm)")
+    
+    generateScatterPlot(data, 2, 3, axis[2][1], clusters, centroids)
+    axis[2][1].set_xlabel("Długość płatka (cm)") 
+    axis[2][1].set_ylabel("Szerokość płatka (cm)")
+    
+    plt.show()
+    
+    return 0;
+
+main()
