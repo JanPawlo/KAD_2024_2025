607d5652669266c984126989214cf03fc3acc0ef
Emil Franczak <251511@edu.p.lodz.pl>
2025-01-18 16:54:05 +0100

    Added fileLoader

--- zadanie_3/zadanie_3.py
+++ zadanie_3/zadanie_3.py
@@ -1,5 +1,24 @@
 import math
 
+# Opens a .csv file, reads lines from it and saves it into an array as float values
+# @path - relative file path to the data
+# returns - two dimensional array of float values
+def fileLoader(path:str):
+    
+    file = open(path, 'r', newline='\n')    
+    dataTable = [];
+
+    i = 0;
+    for x in file:
+        dataTable.append(x.split(','))
+        for j in range(5):
+            dataTable[i][j] = float(dataTable[i][j])
+        i += 1
+    
+    file.close()
+    
+    return dataTable
+
 # calculates euclideanDistance between 2 points located in ANY ammount dimensions
 # @param p1, p2 - lists [x, y, z, ... ]
 # @return float
@@ -24,13 +43,17 @@ def euclideanDistance(p1:list, p2:list) -> float:
 # @param point - classified object
 def kNearestNeighbours(data:list, k:int, point:list):
     
-    # 1. Calculate the Euclidean distance between p and all points in the dataset, storing the results in the list L.
-    listL = []
+    L = []
+    nearestNeighbours = []
     
+    # 1. Calculate the Euclidean distance between p and all points in the dataset, storing the results in the list L.    
     for x in data:
-        listL.append(euclideanDistance(x, point))
+        L.append(euclideanDistance(x[:4], point)) 
     
     # 2. Identify k points in L with the smallest distance from p.
-    # 3. Determine the most common class among the k closest points.
+    # nearestNeighbours = findNearestNeighbours(data, k, L)
+    
+    # 3. Determine the most common class among the k of nearest neighbours.
+    # ClassMembership = determineClassMembership(nearestNeighbours) 
     
     return 0;
